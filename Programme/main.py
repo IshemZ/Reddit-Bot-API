00cb4cd2d563bcadcc2ca4d23e6f425cf8e219fe
Ishem <123887246+IshemZ@users.noreply.github.com>
2024-06-11 20:48:26 +0200

    générer le titre d'un subreddit selon le commentaire

--- Programme/main.py
+++ Programme/main.py
@@ -8,18 +8,12 @@ reddit_instance = praw.Reddit(
     password = password,
     user_agent = "slack-bot"
 )
-
 #print(reddit_instance.user.me())
-
 #subreddit = reddit_instance.subreddit("learnpython")
-
 #print(subreddit)
-
 #top_25_submissions = subreddit.hot(limit=25)
-
 #for submissions in top_25_submissions:
 #    print(submissions.title)
-
 #subreddit = reddit_instance.subreddit("testingground4bots")
 #subreddit.submit(title="This is a test", selftext="ceci est mon corps de texte ?" )
 
